81cd64f9341afb3cc793693bb96f5fa1ce9adf98
Christopher Hsu <cjhsu@unix2.andrew.cmu.edu>
2013-12-05 00:14:32 -0500

    changes

--- handin/src/dns/nameserver.c
+++ handin/src/dns/nameserver.c
@@ -7,8 +7,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include "dns_utils.h"
 #include "roundrobin.h"
+#include "log.h"
 
 #define MAX_FILENAME 128
 #define MAX_UDP_BUF 512
@@ -31,14 +33,14 @@ int read_inputs(int argc, char **argv) {
     }
     idx = 1;
   }
-  
+
   strcpy(logfile, argv[1+idx]);
   strcpy(listen_ip, argv[2+idx]);
   sscanf(argv[3+idx], "%d", &listen_port);
-  
+
   strcpy(servers_file, argv[4+idx]);
   strcpy(lsas_file, argv[5+idx]);
-  
+
   return 1;
 }
 
@@ -55,40 +57,40 @@ void send_udp(int sock, char *buf, int len, struct sockaddr_in *dest) {
 
 void send_error_udp(int sock, struct sockaddr_in *dest, byte rcode) {
   dns_header *dh = dns_create_header(IS_ERROR, rcode);
-  
+
   char *buf;
   int buflen = dns_make_buf(dh, NULL, NULL, buf);
-  
+
   send_udp(sock, buf, buflen, dest);
-  
+
   free(dh);
   free(buf);
 }
 
 void send_valid_udp(int sock, struct sockaddr_in * dest, char *name) {
-  char ip[INET_ADDRSTRLEN];
+  char *ip;
   int server_ip;
-  
+
   if (roundrobin) {
     ip = rr_next_server();
   } else {
     // LSA
   }
-  
+
   inet_pton(AF_INET, ip, &server_ip);
-  
+
   dns_header *dh = dns_create_header(IS_RESPONSE, R_OK);
   dns_answer *da = dns_create_answer(server_ip);
-  
+
   char *buf;
   int buflen = dns_make_buf(dh, NULL, da, buf);
-  
+
   send_udp(sock, buf, buflen, dest);
-  
+
   char client_ip[INET_ADDRSTRLEN];
   inet_ntop(AF_INET, &(dest->sin_addr.s_addr), client_ip, INET_ADDRSTRLEN);
-  log_print(client_ip , name, ip);
-  
+  log_print(client_ip, name, ip);
+
   free(da);
   free(dh);
   free(buf);
@@ -101,27 +103,27 @@ void process_udp(int sock) {
   struct sockaddr_in from;
   socklen_t fromlen;
   char buf[MAX_UDP_BUF];
-  
-  short bytes_read = recvfrom(sock, buf, MAX_UPD_BUF, 0, (struct sockaddr *) &from, &fromlen);
-  
+
+  short bytes_read = recvfrom(sock, buf, MAX_UDP_BUF, 0, (struct sockaddr *) &from, &fromlen);
+
   if (bytes_read <= 0 ||
       bytes_read < (short) sizeof(dns_header)) {
     fprintf(stderr, "Error in process_udp!\n");
     return;
   }
-  
+
   dns_header dh;
   dns_process_header(buf, &dh);
-  
-  if (ntohs(dh->qdcount) != 1) {
+
+  if (ntohs(dh.qdcount) != 1) {
     send_error_udp(sock, &from, R_FORMAT);
     return;
   }
-  
+
   dns_question dq;
   dns_process_query(buf + sizeof(dns_header), &dq);
   if (dns_is_valid(&dh, &dq)) {
-    char *name = dns_query_name(dq);
+    char *name = dns_query_name(&dq);
     if (strcmp("video.cs.cmu.edu", name) == 0) {
       send_valid_udp(sock, &from, name);
     } else {
@@ -137,31 +139,31 @@ void select_loop() {
   int sock;
   struct sockaddr_in myaddr;
   fd_set readfds;
-  
+
   if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP)) == -1) {
     fprintf(stderr, "Could not create socket!\n");
     return;
   }
-  
+
   bzero(&myaddr, sizeof(myaddr));
   myaddr.sin_family = AF_INET;
   inet_pton(AF_INET, listen_ip, &(myaddr.sin_addr));
   myaddr.sin_port = htons(listen_port);
-  
+
   if (bind(sock, (struct sockaddr *) &myaddr, sizeof(myaddr)) == -1) {
     fprintf(stderr, "Could not bind socket!\n");
     return;
   }
-  
+
   FD_ZERO(&readfds);
-  
+
   while (1) {
     int sel_ret;
-    
+
     FD_SET(sock, &readfds);
-    
+
     sel_ret = select(sock+1, &readfds, NULL, NULL, NULL);
-    
+
     if (sel_ret < 0) {
       printf("Select error: %d\n", errno);
       return;
@@ -170,7 +172,7 @@ void select_loop() {
       printf("Select returned 0\n");
       break;
     }
-    
+
     if (FD_ISSET(sock, &readfds)) {
       process_udp(sock);
     }
@@ -181,16 +183,16 @@ int main(int argc, char **argv) {
   if (!read_inputs(argc, argv)) {
     return EXIT_FAILURE;
   }
-  
+
   if (roundrobin) {
     rr_parse_servers(servers_file);
   } else {
     // LSA
   }
-  
+
   select_loop();
-  
+
   cleanup();
-  
+
   return EXIT_SUCCESS;
 }
